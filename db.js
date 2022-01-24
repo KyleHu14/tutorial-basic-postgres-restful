81de42436ad85d44c137b02b94a13c8f7013b7be
KyleHu14 <82713686+KyleHu14@users.noreply.github.com>
2022-01-24 14:16:42 -0800

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     // These can be stored in a env file 
     user: "postgres",
-    password: "1688",
+    password: "postgres",
     database: "todo_basic",
     host: "localhost",
     port: 5432
